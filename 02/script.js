97392b194636c54fefdebb4655d441db0f603f8a
yayan <githabaja@gmail.com>
2025-11-06 18:56:06 +0700

    menambahkan fungsi untuk mengubah teks dari file eksternal

---
+++ 02/script.js
@@ -0,0 +1,7 @@
+// Fungsi untuk mengubah teks dari file eksternal
+function ubahDariEksternal() {
+  const judul2 = document.getElementById("judul2");
+  judul2.innerText = "2️⃣ Halo dari File Eksternal! 👾";
+  judul2.style.color = "#00ffea";
+  judul2.style.textShadow = "0 0 20px #00ffee";
+}
