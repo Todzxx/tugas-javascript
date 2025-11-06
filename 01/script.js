3def11ee6ad4b92bdc9abda1847233dc7cfede39
yayan <githabaja@gmail.com>
2025-11-06 18:33:14 +0700

    menambahkan fungsi ubahTeks untuk mengganti teks elemen dengan id 'judul'

---
+++ 01/script.js
@@ -0,0 +1,6 @@
+// Fungsi yang dipanggil saat tombol diklik
+function ubahTeks() {
+  // Mengambil elemen HTML dengan id 'judul'
+  // lalu mengganti teksnya
+  document.getElementById("judul").innerText = "Teksnya berubah!";
+}
